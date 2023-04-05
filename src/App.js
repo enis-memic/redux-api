62cd0d2600061df079e43252d3dec482f8de6800
Enis <enis.mema@gmail.com>
2023-04-05 16:17:36 +0200

    Redux api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FetchedUsers from './component/fetchedUsers';
+function App() {
+  return (
+    <div className="App">
+      <h1>What's up</h1>
+      <FetchedUsers />
+    </div>
+  );
+}
+
+export default App;
